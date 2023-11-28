19887a8ac117c07b53477b403f372d9acfe42672
Piyabud <piyabud.d@kkumail.com>
2023-11-28 15:21:58 +0700

    add

--- P5.py
+++ P5.py
@@ -1,8 +1,8 @@
-def power_equiv(watt):
+def power_equiv(pwatt):
     hp = pwatt/745.7
     btum = pwatt/0.293
     calm = pwatt*14.33
-    erg = pwatt*1e7
+    erg = pwatt*(1e7)
     flbm = pwatt*44.25
     return hp, btum, calm, erg, flbm
 
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     pwatt = 100
     horsep, btuh, calmin, perg, footlbm = power_equiv(pwatt)
     print(pwatt, 'w =', round(horsep,3), 'hp =',
-        round(btuh,3), 'btu/h =',
-        round(calmin,3),'cal/min =',
-        round(perg,3), 'erg =',
-        round(footlbm,3), 'f-lb/min')
+    round(btuh,3), 'btu/h =',
+    round(calmin,3),'cal/min =',
+    round(perg,3), 'erg =',
+    round(footlbm,3), 'f-lb/min')
--- P6.py
+++ P6.py
@@ -1,12 +1,11 @@
 a = float(input("a: "))
 b = float(input("b: "))
 if a == 0 and b == 0:
-    print("a/b = Not definded")
-elif a != 0 and b == 0 :
-    if a > 0 :
+    print("a/b = Not defined")
+elif a > 0 and b == 0 :
         print("a/b = Infinity")
-    elif a < 0 :
-        print("a/b = Negative Infinity")
+elif a < 0 and b == 0 :
+        print("a/b = Negative infinity")
 else :
     r = a/b
     print("a/b =",r)
